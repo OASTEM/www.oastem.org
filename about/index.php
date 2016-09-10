9428a40c5bf6b6175db6dac144e047d8d3c956c8
Brandon Nguyen <bnguyen9469@yahoo.com>
2016-09-10 13:48:35 -0700

    About page

--- about/index.php
+++ about/index.php
@@ -18,13 +18,15 @@ experience in the STEM fields, be it by engineering robots, studying targeted sc
     </div>
 </div>
 
-<div class="row about-dept-wrap">
+<div class="ab-dept-img" id="ab-sci-img"></div>
+
+<div class="row about-dept-wrap sci-gr">
     <div class="col-4 dept-right">
-        <img class="ab-icon" src="/images/Green-Atom.svg">
+        <img class="ab-icon" src="/images/Atom.svg">
     </div>
     <div class="col-8 dept-left">
-        <h2 class="sci-gr">SCIENCE</h2>
-        <h4>we're more than just <span class="sci-gr">weirdos</span></h4>
+        <h2 class="white">SCIENCE</h2>
+        <h4>we're more than just <span class="white">weirdos</span></h4>
         <p>STEM is the sole Science, Technology, Engineering, and Mathematics focused organization on Oxford Academy's campus. Of distinction is OA STEM's hybrid club-team organization: club in that OA STEM consists of a group of like-minded and similarly-interested students meeting for activities, team in that OA STEM takes part in some of the most competitive contests in the nation.</p>
         <p>By expanding on OA STEM's position as the premier, largest, and only STEM-focused team in a school for the most talented students in the state, the team's membership and sponsorship are almost sure to grow.</p>
         <p>OA STEM strives to offer students the chance to learn more about and get hands-on 
@@ -32,13 +34,15 @@ experience in the STEM fields, be it by engineering robots, studying targeted sc
     </div>
 </div>
 
-<div class="row about-dept-wrap">
+<div class="ab-dept-img" id="ab-tch-img"></div>
+
+<div class="row about-dept-wrap tch-bl">
     <div class="col-4 dept-left">
-        <img class="ab-icon" src="/images/Blue-Pointer.svg">
+        <img class="ab-icon" src="/images/Pointer.svg">
     </div>
     <div class="col-8 dept-right">
-        <h2 class="tch-bl">TECHNOLOGY</h2>
-        <h4>we're more than just <span class="tch-bl">weirdos</span></h4>
+        <h2 class="white">TECHNOLOGY</h2>
+        <h4>we're more than just <span class="white">weirdos</span></h4>
         <p>STEM is the sole Science, Technology, Engineering, and Mathematics focused organization on Oxford Academy's campus. Of distinction is OA STEM's hybrid club-team organization: club in that OA STEM consists of a group of like-minded and similarly-interested students meeting for activities, team in that OA STEM takes part in some of the most competitive contests in the nation.</p>
         <p>By expanding on OA STEM's position as the premier, largest, and only STEM-focused team in a school for the most talented students in the state, the team's membership and sponsorship are almost sure to grow.</p>
         <p>OA STEM strives to offer students the chance to learn more about and get hands-on 
@@ -46,13 +50,15 @@ experience in the STEM fields, be it by engineering robots, studying targeted sc
     </div>
 </div>
 
-<div class="row about-dept-wrap">
+<div class="ab-dept-img" id="ab-egr-img"></div>
+
+<div class="row about-dept-wrap egr-or">
     <div class="col-4 dept-right">
-        <img class="ab-icon" src="/images/Orange-Gear.svg">
+        <img class="ab-icon" src="/images/Gear.svg">
     </div>
     <div class="col-8 dept-left">
-        <h2 class="egr-or">ENGINEERING</h2>
-        <h4>we're more than just <span class="egr-or">weirdos</span></h4>
+        <h2 class="white">ENGINEERING</h2>
+        <h4>we're more than just <span class="white">weirdos</span></h4>
         <p>STEM is the sole Science, Technology, Engineering, and Mathematics focused organization on Oxford Academy's campus. Of distinction is OA STEM's hybrid club-team organization: club in that OA STEM consists of a group of like-minded and similarly-interested students meeting for activities, team in that OA STEM takes part in some of the most competitive contests in the nation.</p>
         <p>By expanding on OA STEM's position as the premier, largest, and only STEM-focused team in a school for the most talented students in the state, the team's membership and sponsorship are almost sure to grow.</p>
         <p>OA STEM strives to offer students the chance to learn more about and get hands-on 
@@ -60,13 +66,15 @@ experience in the STEM fields, be it by engineering robots, studying targeted sc
     </div>
 </div>
 
-<div class="row about-dept-wrap">
+<div class="ab-dept-img" id="ab-mth-img"></div>
+
+<div class="row about-dept-wrap mth-ye">
     <div class="col-4 dept-left">
-        <img class="ab-icon" src="/images/Yellow-Pi.svg">
+        <img class="ab-icon" src="/images/`Pi.svg">
     </div>
     <div class="col-8 dept-right">
-        <h2 class="mth-ye">MATHEMATICS</h2>
-        <h4>we're more than just <span class="mth-ye">weirdos</span></h4>
+        <h2 class="white">MATHEMATICS</h2>
+        <h4>we're more than just <span class="white">weirdos</span></h4>
         <p>STEM is the sole Science, Technology, Engineering, and Mathematics focused organization on Oxford Academy's campus. Of distinction is OA STEM's hybrid club-team organization: club in that OA STEM consists of a group of like-minded and similarly-interested students meeting for activities, team in that OA STEM takes part in some of the most competitive contests in the nation.</p>
         <p>By expanding on OA STEM's position as the premier, largest, and only STEM-focused team in a school for the most talented students in the state, the team's membership and sponsorship are almost sure to grow.</p>
         <p>OA STEM strives to offer students the chance to learn more about and get hands-on 
